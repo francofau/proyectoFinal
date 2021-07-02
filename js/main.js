daf4acdaaa70e3175d4ca1bb0c087ef5d8e7dde2
Franco Fauda <63079368+francofau@users.noreply.github.com>
2021-07-02 15:31:23 +0200

    agrego recursos

---
+++ js/main.js
@@ -0,0 +1,102 @@
+;(function () {
+
+	var mobileMenuOutsideClick = function() {
+
+		$(document).click(function (e) {
+	    var container = $("#gtco-offcanvas, .js-gtco-nav-toggle");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	    	$('.js-gtco-nav-toggle').addClass('gtco-nav-white');
+
+	    	if ( $('body').hasClass('offcanvas') ) {
+
+    			$('body').removeClass('offcanvas');
+    			$('.js-gtco-nav-toggle').removeClass('active');
+				
+	    		}	    
+	    	}
+		});
+
+	};
+
+	var formTab = function() {
+
+		$('.tab-menu a').on('click', function(event){
+			var $this = $(this),
+				data = $this.data('tab');
+
+			$('.tab-menu li').removeClass('active');
+			$this.closest('li').addClass('active')
+
+			$('.tab .tab-content-inner').removeClass('active');
+			$this.closest('.tab').find('.tab-content-inner[data-content="'+data+'"]').addClass('active');
+
+			event.preventDefault();
+
+		});
+
+	};
+
+	var dropdown = function() {
+
+		$('.has-dropdown').mouseenter(function(){
+
+			var $this = $(this);
+			$this
+				.find('.dropdown')
+				.css('display', 'block')
+				.addClass('animated-fast fadeInUpMenu');
+
+		}).mouseleave(function(){
+			var $this = $(this);
+
+			$this
+				.find('.dropdown')
+				.css('display', 'none')
+				.removeClass('animated-fast fadeInUpMenu');
+		});
+
+	};
+
+	var goToTop = function() {
+
+		$('.js-gotop').on('click', function(event){
+			
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $('html').offset().top
+			}, 500, 'easeInOutExpo');
+			
+			return false;
+		});
+
+		$(window).scroll(function(){
+
+			var $win = $(window);
+			if ($win.scrollTop() > 200) {
+				$('.js-top').addClass('active');
+			} else {
+				$('.js-top').removeClass('active');
+			}
+
+		});
+	
+	};
+	
+	var tNavBar = function(){
+		$("#traerNavBar").load("../incluir/navBar.html");
+	}
+
+	var tFooter = function(){
+		$("#traerFooter").load("../incluir/footer.html");
+	}
+	
+	$(function(){
+		mobileMenuOutsideClick();
+		formTab();
+		dropdown();
+		goToTop();
+		tFooter();
+		tNavBar()
+	});
+}());
