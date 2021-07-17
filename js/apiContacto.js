73649d4ebc19992e3436e7c2e1853fab1ed5aaaf
Franco Fauda <63079368+francofau@users.noreply.github.com>
2021-07-17 09:29:52 +0200

    Corrección de errores Ajax

--- js/apiContacto.js
+++ js/apiContacto.js
@@ -2,49 +2,28 @@ $(document).ready(function () {
     // Evento Click del botón "enviar consulta"
     $('#login').click(function (e) { 
         e.preventDefault(); 
-            nombre = $('nombre').val(),
-            apellido = $('apellido').val(),
-            email = $('email').val(),
-            telefono = $('telefono').val(),
-            mens = $('mensaje').val(),
-            adjunt = $('adjunt').val(),
-            enContacto = $('enContacto');/* esto contiene el mensaje que en principio estaría oculto */
-        
-        // Validar que los siguientes campos estén completados:
-        if(nombre.length == 0 || apellido.length == 0 || email.length == 0 || telefono.length == 0 || mens.length == 0) msg_error.show().html("Complete los datos obligatorios");  
-        else{
-            
-            enContacto.hide();
-
+    
             // Arreglo con los datos para enviar
-            var Data = {
-                "nombre": nombre,
-                "apellido": apellido,
-                "email": email,
-                "telefono": telefono,
-                "mensaje": mens,
-                "adjunt":adjunt
+            var datos = {
+                nombre:$('#nombre').val(),
+                apellido:$('#apellido').val(),
+                email:$('#email').val(),
+                telefono:$('#telefono').val(),
+                mens: $('#mensaje').val(),
+                adjunt: $('#adjunt').val()
             };
 
-            // AJAX para conectarnos con la API
             $.ajax({
-                type: "POST",
-                url: '128...',
-                data: Data,
+                type:"POST",
+                url:"http://127.0.0.1:8000/api/login",
+                data:datos,
                 dataType: 'JSON',
-                success: function (data) {
-                    enContacto.show();/* muestra el mensaje inferior */                    
-                    $('#nombre').val('');
-                    $('#apellido').val('');
-                    $('#email').val('');
-                    $('#telefono').val('');
-                    $('#password').val('');
-                    $('#adjunt').val('');                    
-                    },
-                error: function(error){
-                   enContacto.show(error)
-                }                
-            });
-        }
+                success:function(datos){ //success es una funcion que se utiliza si el servidor retorna informacion
+                   console.log('exito')
+                },
+                 error: function(error){
+                     console.log('final');
+                } 
+            })       
     });
 });
