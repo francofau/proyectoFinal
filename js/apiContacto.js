5df2fa5eec1b2cda1c1c97e6bb18a514cc90c84f
Franco Fauda <63079368+francofau@users.noreply.github.com>
2021-07-18 17:50:52 +0200

    validación de campos completados
    mediante JQueryValidator

--- js/apiContacto.js
+++ js/apiContacto.js
@@ -1,4 +1,24 @@
 $(document).ready(function () {
+/*     funcion de JqueryValidate que verifica y valida los campos según las siguientes opciones */
+     $("#formContacto").validate({
+    rules: {
+            nombre: {required: true,minlength: 3},
+            apellido: {required: true,minlength: 3},
+            email: {required: true,email: true},
+            telefono: {required: true,number: true,min: 7},
+            mensaje: { required:true,min: 4},
+        },
+    messages: {
+            nombre: {required: "Ingrese su nombre por favor",minlength: "Por favor ingrese un nombre válido"},
+            apellido: {required: "Ingrese su apellido por favor",minlength: "Por favor ingrese un apellido válido"},
+            email: {email: "Respetar el siguiente formato: email@dominio.com",required: "Ingrese una dirección de correo electrónico"},
+            telefono: {required: "Ingrese un número de teléfono por favor",minlength: "Revise el teléfono ingresado",
+            number: "Ingrese sólo números"},
+            mensaje: {required: "Por favor complete la descripción"}
+        },
+        errorElement: 'span'
+    });
+    
     // Evento Click del botón "enviar consulta"
     $('#login').click(function (e) { 
         e.preventDefault(); 
@@ -22,8 +42,26 @@ $(document).ready(function () {
                    console.log('exito')
                 },
                  error: function(error){
-                     console.log('final');
+                     console.log('error');
                 } 
             })       
+
+  
+            /* Alerta personalizada que desaparece según el tiempo indicado */
+            swal({
+                    icon: "success",
+                    title: "Datos enviados",
+                    text: "En breve nos pondremos en contacto",
+                    buttons: false,
+                    timer: 2000,
+
+            });
+            /*Una vez presionado el botón y mostrado el mensaje superior, se limpian los campos del formulario */
+            $("#formContacto")[0].reset();
+
+            
     });
+    
 });
+
+
